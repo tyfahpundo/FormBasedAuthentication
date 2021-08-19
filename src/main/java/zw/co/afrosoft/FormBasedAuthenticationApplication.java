d65dc9df29eabcd55005ecd2cfa3de7cf09302eb
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-08-19 05:23:36 +0200

    Initial Commit

---
+++ src/main/java/zw/co/afrosoft/FormBasedAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormBasedAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormBasedAuthenticationApplication.class, args);
+    }
+
+}
